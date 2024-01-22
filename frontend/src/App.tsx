91bb4abbea4746fef86af9d75e3a552bfc6146fd
Ben Daniels <benjamindaniels21@gmail.com>
2024-01-21 19:40:08 -0500

    setting up backend connection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
-import './App.css';
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 function App() {
+  const [message, setMessage] = useState('');
+  
+  useEffect(() => {
+    axios.get('http://localhost:3000')
+      .then(response => {
+        console.log(response);
+        setMessage(response.data);
+      })
+      .catch(error => {
+        console.error("error", error);
+      });
+  }, []);
+  
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div>
+      <h1>{message}</h1>
+      <h2>hey</h2>
     </div>
   );
 }
-
 export default App;
