448db2a3137c06b04fd7c14857ce6d49c48f2a3f
Ben Daniels <benjamindaniels21@gmail.com>
2024-01-28 17:37:20 -0500

    add chakra ui elements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,46 +1,64 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import Header from './components/Header';
-import { addToDo, getAllToDo, updateToDo } from "./utils/HandleApi";
+import { Box, Button, ChakraProvider } from "@chakra-ui/react";
 
 
 function App() {
-  const [toDo, setToDo] = useState([]);
-  const [message, setMessage] = useState('');
-  const [text, setText] = useState('')
-  const [isUpdating, setIsUpdating] = useState(false);
-
-
-  useEffect(() => {
-    axios.get('http://localhost:3000')
-      .then(response => {
-        console.log(response);
-        setMessage(response.data);
-      })
-      .catch(error => {
-        console.error("error", error);
-      });
-  }, []);
+  
   
   return (
-    <div className='App'>
-      <Header />
-      <div className="container">
-        <div className="top">
-          <input type="text" placeholder='Add task' value={text} onChange={(e) => setText(e.target.value)}/>
-             <div
-            className="add"
-            onClick={
-              isUpdating
-                ? () => updateToDo()
-                : () => addToDo(text, setText, setToDo)
-            }
-          >
-            {isUpdating ? "Update" : "Add"}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    <ChakraProvider>
+      <Box>Hi from App</Box>
+      <Button colorScheme="purple">Button</Button>
+    </ChakraProvider>
+  )
 }
 export default App;
+
+
+
+
+// import { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import Header from './components/Header';
+// import { addToDo, getAllToDo, updateToDo } from "./utils/HandleApi";
+
+
+// function App() {
+//   const [toDo, setToDo] = useState([]);
+//   const [message, setMessage] = useState('');
+//   const [text, setText] = useState('')
+//   const [isUpdating, setIsUpdating] = useState(false);
+
+
+//   useEffect(() => {
+//     axios.get('http://localhost:3000')
+//       .then(response => {
+//         console.log(response);
+//         setMessage(response.data);
+//       })
+//       .catch(error => {
+//         console.error("error", error);
+//       });
+//   }, []);
+  
+//   return (
+//     <div className='App'>
+//       <Header />
+//       <div className="container">
+//         <div className="top">
+//           <input type="text" placeholder='Add task' value={text} onChange={(e) => setText(e.target.value)}/>
+//              <div
+//             className="add"
+//             onClick={
+//               isUpdating
+//                 ? () => updateToDo()
+//                 : () => addToDo(text, setText, setToDo)
+//             }
+//           >
+//             {isUpdating ? "Update" : "Add"}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+// export default App;
