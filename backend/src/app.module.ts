839d25c3543a2fbbe8410dae13edb157585fc12a
Ben Daniels <benjamindaniels21@gmail.com>
2024-01-30 15:10:47 -0500

    adding schema

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { User } from './user/entities/user.entity';
+// import { UserModule } from './user/user.module';
+// import { User } from './user/entities/user.entity';
 import { ConfigModule } from '@nestjs/config';
 // import { AuthModule } from './auth/auth.module';
 
@@ -16,12 +16,12 @@ import { ConfigModule } from '@nestjs/config';
       port: parseInt(process.env.POSTGRES_PORT),
       password: process.env.POSTGRES_PASSWORD,
       username: process.env.POSTGRES_USER,
-      entities: [User],
+      entities: [],
       database: process.env.POSTGRES_DATABASE,
       synchronize: true,
       logging: true,
     }),
-    UserModule,
+    // UserModule,
     // AuthModule,
   ],
   controllers: [AppController],
