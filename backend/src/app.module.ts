4efe83bab56f09757f9b2b97c76be8ef4f32aed1
Ben Daniels <benjamindaniels21@gmail.com>
2024-01-31 18:33:48 -0500

    adding migration and typeorm scripts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,23 +4,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 // import { UserModule } from './user/user.module';
 // import { User } from './user/entities/user.entity';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Name } from './name.entity';
+import typeorm from './config/typeorm';
 // import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.POSTGRES_PORT),
-      password: process.env.POSTGRES_PASSWORD,
-      username: process.env.POSTGRES_USER,
-      entities: [Name],
-      database: process.env.DATABASE_NAME,
-      synchronize: true,
-      logging: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.get('typeorm'),
     }),
     TypeOrmModule.forFeature([Name]),
     // UserModule,
