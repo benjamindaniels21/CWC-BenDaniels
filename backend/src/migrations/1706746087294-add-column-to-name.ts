8fcbb9658a69a313a760fab0ad8f5be91ff388b6
Ben Daniels <benjamindaniels21@gmail.com>
2024-01-31 19:12:20 -0500

    first db migration!!

--- backend/src/migrations/1706746087294-add-column-to-name.ts
+++ backend/src/migrations/1706746087294-add-column-to-name.ts
@@ -1,18 +1,23 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddColumnToName1706746087294 implements MigrationInterface {
-    name = 'AddColumnToName1706746087294'
+  name = 'AddColumnToName1706746087294';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "name" DROP COLUMN "name"`);
-        await queryRunner.query(`ALTER TABLE "name" ADD "first_name" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "name" ADD "last_name" character varying NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "name" DROP COLUMN "name"`);
+    await queryRunner.query(
+      `ALTER TABLE "name" ADD "first_name" character varying NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "name" ADD "last_name" character varying NOT NULL`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "name" DROP COLUMN "last_name"`);
-        await queryRunner.query(`ALTER TABLE "name" DROP COLUMN "first_name"`);
-        await queryRunner.query(`ALTER TABLE "name" ADD "name" character varying NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "name" DROP COLUMN "last_name"`);
+    await queryRunner.query(`ALTER TABLE "name" DROP COLUMN "first_name"`);
+    await queryRunner.query(
+      `ALTER TABLE "name" ADD "name" character varying NOT NULL`,
+    );
+  }
 }
