7869feb6ba378511efc1649a4725831b112ee9d1
Ben Daniels <benjamindaniels21@gmail.com>
2024-02-07 08:23:34 -0500

    checking entered vs stored password

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -6,11 +6,15 @@ export class AuthService {
   constructor(private usersService: UsersService) {}
   async logIn(username, password) {
     const user = await this.usersService.findUserByUsername(username);
-    console.log(user.password);
+
+    // console.log('user password is:', user.password);
+    // console.log('incoming pass is', password);
+    // console.log(user.password === password);
+
     if (user?.password !== password) {
-      console.log('USER:', user);
+      console.log('incorrect password!');
     } else {
-      console.log('NO USER');
+      console.log(user);
     }
   }
 }
