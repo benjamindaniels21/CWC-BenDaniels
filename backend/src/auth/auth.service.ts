8ce3162da52dbbadecb210fd3dd6728b915f9185
Ben Daniels <benjamindaniels21@gmail.com>
2024-02-08 18:50:22 -0500

    updates!

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,17 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
   async logIn(username, password) {
     const user = await this.usersService.findUserByUsername(username);
 
-    if (user?.password !== password) {
-      console.log('incorrect password!');
+    //check if user exists
+    if (user !== null) {
+      //user exists...compare passwords
+      const passwordsMatch = await bcrypt.compare(password, user.password);
+      if (!passwordsMatch) {
+        throw new UnauthorizedException();
+      }
+      const payload = { sub: user.id, username: user.username };
+      return {
+        access_token: await this.jwtService.signAsync(payload),
+      };
     } else {
-      console.log('correct');
+      console.log('NO USER');
     }
   }
 
