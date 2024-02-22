413c90168532db89181dae8f47bf947f1cd3c1e7
Ben Daniels <benjamindaniels21@gmail.com>
2024-02-21 20:03:23 -0500

    using a jwt

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,11 +3,17 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import jwt from 'src/config/jwt';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [jwt],
+    }),
     UsersModule,
     JwtModule.register({
       global: true,
